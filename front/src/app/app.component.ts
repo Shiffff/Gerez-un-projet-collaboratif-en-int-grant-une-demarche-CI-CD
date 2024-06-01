623fd47526916a996dd80b5dee39db1c8f5ae281
shiffff <alexis.privat@live.fr>
2024-06-01 17:10:53 +0200

    tes

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Joke } from './model/joke.model';
 import { JokesService } from './services/jokes.service';
@@ -6,14 +6,12 @@ import { JokesService } from './services/jokes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   public joke$: Observable<Joke | null> = this.jokesService.joke$();
 
-  constructor(private jokesService: JokesService) {
-  }
+  constructor(private jokesService: JokesService) {}
 
   public ngOnInit(): void {
     this.getRandomJoke();
@@ -21,5 +19,25 @@ export class AppComponent implements OnInit {
 
   public getRandomJoke(): void {
     this.jokesService.getRandomJoke();
+  }
+
+  public add(a: number, b: number): number {
+    return a + b;
+  }
+
+  public factorial(n: number): number {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+      result = result * i;
+    }
+    return result;
+  }
+  public celsiusToFahrenheit(celsius: number): number {
+    const fahrenheit = (celsius * 9) / 5 + 32;
+    return fahrenheit;
+  }
+
+  public isEven(n: number): boolean {
+    return n % 2 === 0;
   }
 }
